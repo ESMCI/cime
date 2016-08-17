6c6ef5ad09a552b652f26789ce4b8a7e6b883ecd
Erik Kluzek <erik@ucar.edu>
2016-08-17 15:51:22 -0600

    Add Sl_frac_irrig and Flrr_irrig

--- driver_cpl/shr/seq_flds_mod.F90
+++ driver_cpl/shr/seq_flds_mod.F90
@@ -1086,6 +1086,15 @@ module seq_flds_mod
      attname  = 'Sl_snowh'
      call metadata_set(attname, longname, stdname, units)
 
+     ! Irrigation fraction (land/rof only) 
+     call seq_flds_add(l2x_states,"Sl_frac_irrig")
+     call seq_flds_add(x2a_states,"Sl_frac_irrig")
+     longname = 'Irrigation fraction'
+     stdname  = 'irrigation_fraction'
+     units    = '1'
+     attname  = 'Sl_frac_irrig'
+     call metadata_set(attname, longname, stdname, units)
+
      ! Surface snow depth (ice/atm only)
      call seq_flds_add(i2x_states,"Si_snowh")
      call seq_flds_add(x2a_states,"Si_snowh")
@@ -1518,6 +1527,14 @@ module seq_flds_mod
      stdname  = 'flooding_water_flux'
      units    = 'kg m-2 s-1'
      attname  = 'Flrr_flood'
+     call metadata_set(attname, longname, stdname, units)
+
+     call seq_flds_add(r2x_fluxes,'Flrr_irrig')
+     call seq_flds_add(x2l_fluxes,'Flrr_irrig')
+     longname = 'Waterrflux due to irrigation'
+     stdname  = 'irrigation_water_flux'
+     units    = 'kg m-2 s-1'
+     attname  = 'Flrr_irrig'
      call metadata_set(attname, longname, stdname, units)
 
      call seq_flds_add(r2x_fluxes,'Flrr_volr')
