201aa49afeb207900d1e18268bffbaac024e8a99
Ed Hartnett <edwardjameshartnett@gmail.com>
2016-09-14 13:48:14 -0600

    more shared test code

--- tests/unit/test_intercomm4.c
+++ tests/unit/test_intercomm4.c
@@ -16,78 +16,38 @@
 /* Number of computational components to create. */
 #define COMPONENT_COUNT 1
 
+/* The number of tasks this test should run on. */
+#define TARGET_NTASKS 2
+
 /** The number of possible output netCDF output flavors available to
  * the ParallelIO library. */
 #define NUM_NETCDF_FLAVORS 4
 
 /** Run Tests for Init_Intercomm
- *
- * @param argc argument count
- * @param argv array of arguments
  */
 int
 main(int argc, char **argv)
 {
-    /** Zero-based rank of processor. */
-    int my_rank;
-
-    /** Number of processors involved in current execution. */
-    int ntasks;
-
-    /** Different output flavors. */
-    int format[NUM_NETCDF_FLAVORS] = {PIO_IOTYPE_PNETCDF,
+    int my_rank; /* Zero-based rank of processor. */
+    int ntasks; /* Number of processors involved in current execution. */
+    int iosysid[COMPONENT_COUNT]; /* The ID for the parallel I/O system. */
+    int fmt; /* Index for loop of PIO netcdf formats. */
+    int ret; /* Return code. */
+    int format[NUM_NETCDF_FLAVORS] = {PIO_IOTYPE_PNETCDF,  /* Output flavors. */
 				      PIO_IOTYPE_NETCDF,
 				      PIO_IOTYPE_NETCDF4C,
 				      PIO_IOTYPE_NETCDF4P};
-
-    /** Names for the output files. */
     char base_filename[NUM_NETCDF_FLAVORS][NC_MAX_NAME + 1] = {"test_intercomm4_pnetcdf",
 							       "test_intercomm4_classic",
 							       "test_intercomm4_serial4",
 							       "test_intercomm4_parallel4"};
+    int num_procs[COMPONENT_COUNT + 1] = {1, 1}; /* Num procs for IO and computation. */
 
-    /** The ID for the parallel I/O system. */
-    int iosysid[COMPONENT_COUNT];
 
-    /** Return code. */
-    int ret;
-
-    /** Index for loops. */
-    int fmt, d, d1, i;
-
-#ifdef TIMING
-    /* Initialize the GPTL timing library. */
-    if ((ret = GPTLinitialize ()))
-	return ret;
-#endif
-
-    /* Initialize MPI. */
-    if ((ret = MPI_Init(&argc, &argv)))
-	MPIERR(ret);
-
-    /* Learn my rank and the total number of processors. */
-    if ((ret = MPI_Comm_rank(MPI_COMM_WORLD, &my_rank)))
-	MPIERR(ret);
-    if ((ret = MPI_Comm_size(MPI_COMM_WORLD, &ntasks)))
-	MPIERR(ret);
-
-    /* Check that a valid number of processors was specified. */
-    if (ntasks != 2)
-    {
-	fprintf(stderr, "test_intercomm4 Number of processors must be exactly 2!\n");
+    /* Initialize test. */
+    if ((ret = pio_test_init(argc, argv, &my_rank, &ntasks, TARGET_NTASKS)))
 	ERR(ERR_AWFUL);
-    }
-    printf("%d: test_intercomm4 ParallelIO Library test_intercomm4 running on %d processors.\n",
-	   my_rank, ntasks);
-
-    /* Turn on logging. */
-    if ((ret = PIOc_set_log_level(3)))
-	ERR(ret);
-
-    /* How many processors will be used for our IO and computation
-     * component. */
-    int num_procs[COMPONENT_COUNT + 1] = {1, 1};
-
+    
     /* Is the current process a computation task? */
     int comp_task = my_rank < 1 ? 0 : 1;
 
