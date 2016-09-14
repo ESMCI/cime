67dc776639af611b2f3065ea821e186d2eab1134
Ed Hartnett <edwardjameshartnett@gmail.com>
2016-09-14 13:33:21 -0600

    added test_common.c to share test code

--- tests/unit/test_intercomm4.c
+++ tests/unit/test_intercomm4.c
@@ -20,113 +20,6 @@
  * the ParallelIO library. */
 #define NUM_NETCDF_FLAVORS 4
 
-/** The number of dimensions in the test data. */
-#define NDIM 1
-
-/** The length of our test data. */
-#define DIM_LEN 4
-
-/** The name of the dimension in the netCDF output file. */
-#define FIRST_DIM_NAME "jojo"
-#define DIM_NAME "dim_test_intercomm3"
-
-/** The name of the variable in the netCDF output file. */
-#define FIRST_VAR_NAME "bill"
-#define VAR_NAME "var_test_intercomm3"
-
-
-/* Check the file for correctness. */
-int
-check_file(int iosysid, int format, char *filename, int my_rank)
-{
-    int ncid;
-    int ret;
-    int ndims, nvars, ngatts, unlimdimid;
-    int ndims2, nvars2, ngatts2, unlimdimid2;
-    int dimid2;
-    char dimname[NC_MAX_NAME + 1];
-    PIO_Offset dimlen;
-    char dimname2[NC_MAX_NAME + 1];
-    PIO_Offset dimlen2;
-    char varname[NC_MAX_NAME + 1];
-    nc_type vartype;
-    int varndims, vardimids, varnatts;
-    char varname2[NC_MAX_NAME + 1];
-    nc_type vartype2;
-    int varndims2, vardimids2, varnatts2;
-    int varid2;
-    int att_data;
-    short short_att_data;
-    float float_att_data;
-    double double_att_data;
-
-    /* Re-open the file to check it. */
-    printf("%d test_intercomm3 opening file %s format %d\n", my_rank, filename, format);
-    if ((ret = PIOc_openfile(iosysid, &ncid, &format, filename,
-    			     NC_NOWRITE)))
-    	ERR(ret);
-
-    /* Try to read the data. */
-    PIO_Offset start[NDIM] = {0}, count[NDIM] = {DIM_LEN};
-    int data_in[DIM_LEN];
-    if ((ret = PIOc_get_vars_tc(ncid, 0, start, count, NULL, NC_INT, data_in)))
-    	ERR(ret);
-    for (int i = 0; i < DIM_LEN; i++)
-    {
-    	printf("%d test_intercomm3 read data_in[%d] = %d\n", my_rank, i, data_in[i]);
-    	if (data_in[i] != i)
-    	    ERR(ERR_AWFUL);
-    }
-
-    /* Find the number of dimensions, variables, and global attributes.*/
-    if ((ret = PIOc_inq(ncid, &ndims, &nvars, &ngatts, &unlimdimid)))
-    	ERR(ret);
-    if (ndims != 1 || nvars != 1 || ngatts != 0 || unlimdimid != -1)
-    	ERR(ERR_WRONG);
-
-    /* This should return PIO_NOERR. */
-    if ((ret = PIOc_inq(ncid, NULL, NULL, NULL, NULL)))
-    	ERR(ret);
-
-    /* Check the other functions that get these values. */
-    if ((ret = PIOc_inq_ndims(ncid, &ndims2)))
-    	ERR(ret);
-    if (ndims2 != 1)
-    	ERR(ERR_WRONG);
-    if ((ret = PIOc_inq_nvars(ncid, &nvars2)))
-    	ERR(ret);
-    if (nvars2 != 1)
-    	ERR(ERR_WRONG);
-    if ((ret = PIOc_inq_natts(ncid, &ngatts2)))
-    	ERR(ret);
-    if (ngatts2 != 0)
-    	ERR(ERR_WRONG);
-    if ((ret = PIOc_inq_unlimdim(ncid, &unlimdimid2)))
-    	ERR(ret);
-    if (unlimdimid != -1)
-    	ERR(ERR_WRONG);
-
-    /* Check out the dimension. */
-    if ((ret = PIOc_inq_dim(ncid, 0, dimname, &dimlen)))
-    	ERR(ret);
-    if (strcmp(dimname, DIM_NAME) || dimlen != DIM_LEN)
-    	ERR(ERR_WRONG);
-
-    /* Check out the variable. */
-    if ((ret = PIOc_inq_var(ncid, 0, varname, &vartype, &varndims, &vardimids, &varnatts)))
-    	ERR(ret);
-    if (strcmp(varname, VAR_NAME) || vartype != NC_INT || varndims != NDIM ||
-    	vardimids != 0 || varnatts != 0)
-    	ERR(ERR_WRONG);
-
-    /* Close the file. */
-    printf("%d test_intercomm3 closing file (again) ncid = %d\n", my_rank, ncid);
-    if ((ret = PIOc_closefile(ncid)))
-    	ERR(ret);
-
-    return 0;
-}
-
 /** Run Tests for Init_Intercomm
  *
  * @param argc argument count
@@ -155,12 +48,6 @@ main(int argc, char **argv)
 
     /** The ID for the parallel I/O system. */
     int iosysid[COMPONENT_COUNT];
-
-    /** The ncid of the netCDF file. */
-    int ncid;
-
-    /** The ID of the netCDF varable. */
-    int varid;
 
     /** Return code. */
     int ret;
@@ -220,9 +107,6 @@ main(int argc, char **argv)
     {
     	for (int fmt = 0; fmt < NUM_NETCDF_FLAVORS; fmt++)
     	{
-    	    int ncid, varid, dimid;
-    	    PIO_Offset start[NDIM], count[NDIM] = {0};
-    	    int data[DIM_LEN];
 	    char filename[NC_MAX_NAME + 1];
 
 	    /* Create a filename for this computation component. */
@@ -230,59 +114,13 @@ main(int argc, char **argv)
 
     	    /* Create a netCDF file with one dimension and one variable. */
 	    printf("%d test_intercomm4 creating file %s\n", my_rank, filename);
-    	    if ((ret = PIOc_createfile(iosysid[my_comp_idx], &ncid, &format[fmt], filename,
-    	    			       NC_CLOBBER)))
-    	    	ERR(ret);
-	    printf("%d test_intercomm4 file created ncid = %d\n", my_rank, ncid);
 
-    	    /* /\* End define mode, then re-enter it. *\/ */
-    	    if ((ret = PIOc_enddef(ncid)))
-    	    	ERR(ret);
-	    printf("%d test_intercomm4 calling redef\n", my_rank);
-    	    if ((ret = PIOc_redef(ncid)))
-    	    	ERR(ret);
-
-    	    /* Define a dimension. */
-    	    char dimname2[NC_MAX_NAME + 1];
-	    printf("%d test_intercomm4 defining dimension %s\n", my_rank, DIM_NAME);
-    	    if ((ret = PIOc_def_dim(ncid, DIM_NAME, DIM_LEN, &dimid)))
-    	    	ERR(ret);
-
-    	    /* Define a 1-D variable. */
-    	    char varname2[NC_MAX_NAME + 1];
-	    printf("%d test_intercomm4 defining variable %s\n", my_rank, VAR_NAME);
-    	    if ((ret = PIOc_def_var(ncid, VAR_NAME, NC_INT, NDIM, &dimid, &varid)))
-    	    	ERR(ret);
-
-    	    /* char *buf111 = malloc(19999); */
-
-    	    /* End define mode. */
-	    printf("%d test_intercomm4 ending define mode ncid = %d\n", my_rank, ncid);
-    	    if ((ret = PIOc_enddef(ncid)))
-    	    	ERR(ret);
-	    printf("%d test_intercomm4 define mode ended ncid = %d\n", my_rank, ncid);
-
-    	    /* Write some data. For the PIOc_put/get functions, all
-    	     * data must be on compmaster before the function is
-    	     * called. Only compmaster's arguments are passed to the
-    	     * async msg handler. All other computation tasks are
-    	     * ignored. */
-    	    for (int i = 0; i < DIM_LEN; i++)
-    	    	data[i] = i;
-	    printf("%d test_intercomm4 writing data\n", my_rank);
-    	    start[0] = 0;
-    	    count[0] = DIM_LEN;
-    	    if ((ret = PIOc_put_vars_tc(ncid, varid, start, count, NULL, NC_INT, data)))
-    	    	ERR(ret);
-
-    	    /* Close the file. */
-	    printf("%d test_intercomm4 closing file ncid = %d\n", my_rank, ncid);
-    	    if ((ret = PIOc_closefile(ncid)))
-    	    	ERR(ret);
-	    printf("%d test_intercomm4 closed file ncid = %d\n", my_rank, ncid);
+	    /* Create the sample file. */
+	    if ((ret = create_nc_sample_1(iosysid[my_comp_idx], format[fmt], filename, my_rank)))
+		ERR(ret);
 
     	    /* Check the file for correctness. */
-    	    if ((ret = check_file(iosysid[my_comp_idx], format[fmt], filename, my_rank)))
+    	    if ((ret = check_nc_sample_1(iosysid[my_comp_idx], format[fmt], filename, my_rank)))
     	    	ERR(ret);
 
     	} /* next netcdf format flavor */
