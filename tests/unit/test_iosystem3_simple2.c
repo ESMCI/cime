122ff4fcc83ec42a12761855673812eadfecf2a9
Ed Hartnett <edwardjameshartnett@gmail.com>
2016-10-07 14:10:07 -0600

    simplifying test

--- tests/unit/test_iosystem3_simple2.c
+++ tests/unit/test_iosystem3_simple2.c
@@ -16,59 +16,11 @@
 /* The name of this test. */
 #define TEST_NAME "test_iosystem3"
 
-/* Used to define netcdf test file. */
-#define PIO_TF_MAX_STR_LEN 100
-#define ATTNAME "filename"
-#define DIMNAME "filename_dim"
-
 /* Used when initializing PIO. */
 #define STRIDE1 1
-#define STRIDE2 2
 #define BASE0 0
-#define BASE1 1
-#define NUM_IO1 1
-#define NUM_IO2 2
 #define NUM_IO4 4
 #define REARRANGER 1
-
-/** This checks an already-open netCDF file. */
-int
-check_file(MPI_Comm comm, int iosysid, int format, int ncid, char *filename,
-	   char *attname, char *dimname, int my_rank)
-{
-    int dimid;
-    int ret;
-
-    /* Check the file. */
-    if ((ret = PIOc_inq_dimid(ncid, dimname, &dimid)))
-	return ret;
-    printf("%d dimid = %d\n", my_rank, dimid);
-
-    return PIO_NOERR;
-}
-
-/** This opens and checks a netCDF file. */
-int open_and_check_file(MPI_Comm comm, int iosysid, int iotype, int *ncid, char *fname,
-		    char *attname, char *dimname, int disable_close, int my_rank)
-{
-    int mode = PIO_WRITE;
-    int ret;
-
-    /* Open the file. */
-    if ((ret = PIOc_openfile(iosysid, ncid, &iotype, fname, mode)))
-	return ret;
-
-    /* Check the file. */
-    if ((ret = check_file(comm, iosysid, iotype, *ncid, fname, attname, dimname, my_rank)))
-    	return ret;
-
-    /* Close the file, maybe. */
-    if (!disable_close)
-	if ((ret = PIOc_closefile(*ncid)))
-	    return ret;
-	
-    return PIO_NOERR;
-}
 
 /** Run async tests. */
 int
@@ -76,8 +28,9 @@ main(int argc, char **argv)
 {
     int my_rank; /* Zero-based rank of processor. */
     int ntasks; /* Number of processors involved in current execution. */
-    int iosysid; /* The ID for the parallel I/O system. */
     int iosysid_world; /* The ID for the parallel I/O system. */
+    char fname0[] = "pio_iosys_test_file0.nc";
+    int ncid;
     int ret; /* Return code. */
 
     int iotypes[NUM_FLAVORS] = {PIO_IOTYPE_PNETCDF, PIO_IOTYPE_NETCDF,
@@ -94,50 +47,29 @@ main(int argc, char **argv)
 /*    for (int i = 0; i < NUM_FLAVORS; i++)*/
     for (int i = 2; i < 4; i++)
     {
-    	char fname0[] = "pio_iosys_test_file0.nc";
-    	printf("\n\n%d i = %d\n", my_rank, i);
+	/* Create the file. */
+	if ((ret = PIOc_createfile(iosysid_world, &ncid, &iotypes[i], fname0, NC_CLOBBER)))
+	    return ret;
 
-    	/* if ((ret = create_file(MPI_COMM_WORLD, iosysid_world, iotypes[i], fname0, ATTNAME, */
-    	/* 		       DIMNAME, my_rank))) */
-    	/*     ERR(ret); */
-
-	{
-	    int ncid, varid, dimid;
-	    int ret;
-
-	    /* Create the file. */
-	    if ((ret = PIOc_createfile(iosysid_world, &ncid, &iotypes[i], fname0, NC_CLOBBER)))
-		return ret;
-	    printf("%d file created ncid = %d\n", my_rank, ncid);
-
-	    /* Define a dimension. */
-	    if ((ret = PIOc_def_dim(ncid, DIMNAME, PIO_TF_MAX_STR_LEN, &dimid)))
-		return ret;
-
-	    /* /\* Define a 1-D variable. *\/ */
-	    /* if ((ret = PIOc_def_var(ncid, ATTNAME, NC_CHAR, 1, &dimid, &varid))) */
-	    /* 	return ret; */
-
-	    /* /\* Write an attribute. *\/ */
-	    /* if ((ret = PIOc_put_att_text(ncid, varid, ATTNAME, strlen(fname0), fname0))) */
-	    /* 	return ret; */
-
-	    /* End define mode. */
-	    if ((ret = PIOc_enddef(ncid)))
-		return ret;
-	    printf("%d define mode ended ncid = %d\n", my_rank, ncid);
-
-	    /* Close the file. */
-	    if ((ret = PIOc_closefile(ncid)))
-		return ret;
+	/* End define mode. */
+	if ((ret = PIOc_enddef(ncid)))
+	    return ret;
+	
+	/* Close the file. */
+	if ((ret = PIOc_closefile(ncid)))
+	    return ret;
     
-	}
     	/* Now check the first file from WORLD communicator. */
-    	int ncid;
-    	if ((ret = open_and_check_file(MPI_COMM_WORLD, iosysid_world, iotypes[i], &ncid, fname0,
-    				       ATTNAME, DIMNAME, 1, my_rank)))
-    	    ERR(ret);
+	int mode = PIO_WRITE;
 
+	/* Open the file. */
+	if ((ret = PIOc_openfile(iosysid_world, &ncid, &iotypes[i], fname0, mode)))
+	    return ret;
+
+	/* Check the file. */
+	int ndims;
+	if ((ret = PIOc_inq(ncid, &ndims, NULL, NULL, NULL)))
+	    return ret;
     } /* next iotype */
 
     /* Finalize PIO systems. */
