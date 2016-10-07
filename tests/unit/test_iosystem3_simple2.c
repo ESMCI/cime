5e571fccdc4816d8030f78a2050fe690f4d36003
Ed Hartnett <edwardjameshartnett@gmail.com>
2016-10-07 13:47:12 -0600

    simplifying test

--- tests/unit/test_iosystem3_simple2.c
+++ tests/unit/test_iosystem3_simple2.c
@@ -22,7 +22,6 @@
 #define DIMNAME "filename_dim"
 
 /* Used to devide up the tasks into MPI groups. */
-#define OVERLAP_NUM_RANGES 2
 #define EVEN_NUM_RANGES 1
 
 /* Used when initializing PIO. */
@@ -126,14 +125,11 @@ main(int argc, char **argv)
     int iosysid; /* The ID for the parallel I/O system. */
     int iosysid_world; /* The ID for the parallel I/O system. */
     int even_iosysid; /* The ID for iosystem of even_comm. */
-    int overlap_iosysid; /* The ID for iosystem of even_comm. */
     MPI_Group world_group; /* An MPI group of world. */
     MPI_Group even_group; /* An MPI group of 0 and 2. */
-    MPI_Group overlap_group; /* An MPI group of 0, 1, and 3. */
     MPI_Comm even_comm = MPI_COMM_NULL; /* Communicator for tasks 0, 2 */
-    MPI_Comm overlap_comm = MPI_COMM_NULL; /* Communicator for tasks 0, 1, 2. */
-    int even_rank = -1, overlap_rank = -1; /* Tasks rank in communicator. */
-    int even_size = 0, overlap_size = 0; /* Size of communicator. */
+    int even_rank = -1; /* Tasks rank in communicator. */
+    int even_size = 0; /* Size of communicator. */
     int ret; /* Return code. */
 
     int iotypes[NUM_FLAVORS] = {PIO_IOTYPE_PNETCDF, PIO_IOTYPE_NETCDF,
@@ -151,79 +147,13 @@ main(int argc, char **argv)
     if ((ret = MPI_Comm_group(MPI_COMM_WORLD, &world_group)))
 	ERR(ret);
 
-    /* Create a group with tasks 0 and 2. */
-    int even_ranges[EVEN_NUM_RANGES][3] = {0, 2, 2};
-    if ((ret = MPI_Group_range_incl(world_group, EVEN_NUM_RANGES, even_ranges, &even_group)))
-	ERR(ret);
-
-    /* Create a communicator from the even_group. */
-    if ((ret = MPI_Comm_create(MPI_COMM_WORLD, even_group, &even_comm)))
-	ERR(ret);
-
-    /* Learn my rank and the total number of processors in even group. */
-    if (even_comm != MPI_COMM_NULL)
-    {
-	if ((ret = MPI_Comm_rank(even_comm, &even_rank)))
-	    MPIERR(ret);
-	if ((ret = MPI_Comm_size(even_comm, &even_size)))
-	    MPIERR(ret);
-    }
-    printf("%d even_comm = %d even_rank = %d even_size = %d\n", my_rank,
-	   even_comm, even_rank, even_size);
-
-    /* Create a group with tasks 0, 1, and 3. */
-    int overlap_ranges[OVERLAP_NUM_RANGES][3] = {{0, 0, 1}, {1, 3, 2}};
-    if ((ret = MPI_Group_range_incl(world_group, OVERLAP_NUM_RANGES,
-				    overlap_ranges, &overlap_group)))
-	ERR(ret);
-    
-    /* Create a communicator from the overlap_group. */
-    if ((ret = MPI_Comm_create(MPI_COMM_WORLD, overlap_group, &overlap_comm)))
-	ERR(ret);
-
-    /* Learn my rank and the total number of processors in overlap
-     * group. */
-    if (overlap_comm != MPI_COMM_NULL)
-    {
-	if ((ret = MPI_Comm_rank(overlap_comm, &overlap_rank)))
-	    MPIERR(ret);
-	if ((ret = MPI_Comm_size(overlap_comm, &overlap_size)))
-	    MPIERR(ret);
-    }
-    printf("%d overlap_comm = %d overlap_rank = %d overlap_size = %d\n", my_rank,
-	   overlap_comm, overlap_rank, overlap_size);
-
-    /* Initialize PIO system for even. */
-    if (even_comm != MPI_COMM_NULL)
-    {
-    	if ((ret = PIOc_Init_Intracomm(even_comm, NUM_IO1, STRIDE1, BASE1, REARRANGER, &even_iosysid)))
-    	    ERR(ret);
-    }
-
-    /* Initialize PIO system for overlap comm. */
-    if (overlap_comm != MPI_COMM_NULL)
-    {
-	if ((ret = PIOc_Init_Intracomm(overlap_comm, NUM_IO2, STRIDE1, BASE1, REARRANGER, &overlap_iosysid)))
-	    ERR(ret);
-    }
-
 /*    for (int i = 0; i < NUM_FLAVORS; i++)*/
     for (int i = 2; i < 4; i++)
     {
     	char fname0[] = "pio_iosys_test_file0.nc";
-    	char fname1[] = "pio_iosys_test_file1.nc";
-    	char fname2[] = "pio_iosys_test_file2.nc";
     	printf("\n\n%d i = %d\n", my_rank, i);
 
     	if ((ret = create_file(MPI_COMM_WORLD, iosysid_world, iotypes[i], fname0, ATTNAME,
-    			       DIMNAME, my_rank)))
-    	    ERR(ret);
-
-    	if ((ret = create_file(MPI_COMM_WORLD, iosysid_world, iotypes[i], fname1, ATTNAME,
-    			       DIMNAME, my_rank)))
-    	    ERR(ret);
-
-    	if ((ret = create_file(MPI_COMM_WORLD, iosysid_world, iotypes[i], fname2, ATTNAME,
     			       DIMNAME, my_rank)))
     	    ERR(ret);
 
@@ -235,21 +165,6 @@ main(int argc, char **argv)
     				       ATTNAME, DIMNAME, 1, my_rank)))
     	    ERR(ret);
 
-    	/* /\* Now have the odd/even communicators each check one of the */
-    	/*  * remaining files. *\/ */
-    	/* int ncid2; */
-    	/* char *fname = even ? fname1 : fname2; */
-    	/* printf("\n***\n"); */
-    	/* if ((ret = open_and_check_file(newcomm, iosysid, iotypes[i], &ncid2, fname, */
-    	/* 			       ATTNAME, DIMNAME, 1, my_rank))) */
-    	/*     ERR(ret); */
-	
-
-    	/* /\* Close the still-open files. *\/ */
-    	/* if ((ret = PIOc_closefile(ncid))) */
-    	/*     ERR(ret); */
-    	/* if ((ret = PIOc_closefile(ncid2))) */
-    	/*     ERR(ret); */
     } /* next iotype */
 
     /* Finalize PIO systems. */
@@ -257,27 +172,15 @@ main(int argc, char **argv)
     if (even_comm != MPI_COMM_NULL)
     	if ((ret = PIOc_finalize(even_iosysid)))
     	    ERR(ret);
-    printf("%d pio finalizing %d\n", my_rank, overlap_iosysid);
-    if (overlap_comm != MPI_COMM_NULL)
-    {
-	printf("%d calling PIOc_finalize with iosysid = %d\n", my_rank, overlap_iosysid);
-	if ((ret = PIOc_finalize(overlap_iosysid)))
-	    ERR(ret);
-    }
     printf("%d pio finalized\n", my_rank);
     if ((ret = PIOc_finalize(iosysid_world)))
     	ERR(ret);
 
     /* Free MPI resources used by test. */
-    if ((ret = MPI_Group_free(&overlap_group)))
-	ERR(ret);
     if ((ret = MPI_Group_free(&even_group)))
 	ERR(ret);
     if ((ret = MPI_Group_free(&world_group)))
 	ERR(ret);
-    if (overlap_comm != MPI_COMM_NULL)
-	if ((ret = MPI_Comm_free(&overlap_comm)))
-	    ERR(ret);
     if (even_comm != MPI_COMM_NULL)
     	if ((ret = MPI_Comm_free(&even_comm)))
     	    ERR(ret);
