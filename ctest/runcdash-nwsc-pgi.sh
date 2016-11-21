2a1443303313ed69981c155b419ca83a2b237f60
Jim Edwards <jedwards@ucar.edu>
2016-11-21 14:04:19 -0700

    update modules for cdash test on yellowstone intel

--- ctest/runcdash-nwsc-pgi.sh
+++ ctest/runcdash-nwsc-pgi.sh
@@ -9,10 +9,10 @@ fi
 
 module reset
 module unload netcdf
-module swap intel pgi/15.10
+module swap intel pgi/16.5
 module load git/2.3.0
 module load cmake/3.0.2
-module load netcdf-mpi/4.3.3.1
+module load netcdf-mpi/4.4.1
 module load pnetcdf/1.6.1
 
 export CC=mpicc
