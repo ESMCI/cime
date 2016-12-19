c0744b37fe5bb4165ced045a281d51e43b0ca791
jayeshkrishna <jayesh@mcs.anl.gov>
2016-12-19 10:21:51 -0600

    Remove hardcoded string lens in unit test
    The old fortran unit test used hardcoded lengths for strings used
    to retrieve pio error messages. Now using pio_max_name instead.
    
    See Issue #319

--- tests/unit/driver.F90
+++ tests/unit/driver.F90
@@ -27,8 +27,8 @@ Program pio_unit_test_driver
   integer, external :: nc_set_log_level2
 #endif
   integer ret_val
-  character(len=80) :: errmsg
-  character(len=80) :: expected
+  character(len=pio_max_name) :: errmsg
+  character(len=pio_max_name) :: expected
   
   ! Set up MPI
   call MPI_Init(ierr)
