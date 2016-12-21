bcbd05380a782cc1c344fb054636f4734f03d4d5
Ed Hartnett <edwardjameshartnett@gmail.com>
2016-12-21 13:44:38 -0500

    cleanup of error handling

--- src/clib/pioc_support.c
+++ src/clib/pioc_support.c
@@ -325,8 +325,7 @@ void pioassert(_Bool expression, const char *msg, const char *fname, int line)
  * Handle MPI errors. An error message is sent to stderr, then the
  * check_netcdf() function is called with PIO_EIO.
  *
- * @param file pointer to the file_desc_t info. May be NULL, in which
- * case check_netcdf() is not called.
+ * @param file pointer to the file_desc_t info. Ignored if NULL.
  * @param mpierr the MPI return code to handle
  * @param filename the name of the code file where error occured.
  * @param line the line of code where error occured.
@@ -374,7 +373,8 @@ int check_mpi2(iosystem_desc_t *ios, file_desc_t *file, int mpierr,
 /**
  * Check the result of a netCDF API call.
  *
- * @param file pointer to the PIO structure describing this file.
+ * @param file pointer to the PIO structure describing this
+ * file. Ignored if NULL.
  * @param status the return value from the netCDF call.
  * @param fname the name of the code file.
  * @param line the line number of the netCDF call in the code.
@@ -391,8 +391,8 @@ int check_netcdf(file_desc_t *file, int status, const char *fname, int line)
  * is used to determine error handling when there is no file_desc_t
  * pointer.
  *
- * @param ios the iosystem description struct
- * @param file pointer to the PIO structure describing this file.
+ * @param ios pointer to the iosystem description struct. Ignored if NULL.
+ * @param file pointer to the PIO structure describing this file. Ignored if NULL.
  * @param status the return value from the netCDF call.
  * @param fname the name of the code file.
  * @param line the line number of the netCDF call in the code.
@@ -411,7 +411,7 @@ int check_netcdf2(iosystem_desc_t *ios, file_desc_t *file, int status,
     if (status == PIO_NOERR)
         return PIO_NOERR;
 
-    LOG((1, "check_netcdf4 status = %d fname = %s line = %d", status, fname, line));
+    LOG((1, "check_netcdf2 status = %d fname = %s line = %d", status, fname, line));
 
     /* Pick an error handler. File settings override iosystem
      * settings. */
