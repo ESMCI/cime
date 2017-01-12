cba742c851d4769552b0008c734a08efa7e3e80e
edhartnett <edwardjameshartnett@gmail.com>
2017-01-12 13:52:58 -0700

    more tests

--- tests/cunit/test_pioc.c
+++ tests/cunit/test_pioc.c
@@ -913,7 +913,11 @@ int test_nc4(int iosysid, int num_flavors, int *flavor, int my_rank)
             ERR(ret);
 
         /* Check that invalid arguments are properly rejected. */
+        if (PIOc_def_var_chunking(ncid + 1, 1000, NC_CHUNKED, chunksize) == PIO_NOERR)
+            ERR(ERR_AWFUL);
         if (PIOc_def_var_chunking(ncid + 1, 0, NC_CHUNKED, chunksize) != PIO_EBADID)
+            ERR(ERR_AWFUL);
+        if (PIOc_inq_var_chunking(ncid + 1, 1000, &storage, my_chunksize) == PIO_NOERR)
             ERR(ERR_AWFUL);
         if (PIOc_inq_var_chunking(ncid + 1, 0, &storage, my_chunksize) != PIO_EBADID)
             ERR(ERR_AWFUL);
@@ -941,6 +945,10 @@ int test_nc4(int iosysid, int num_flavors, int *flavor, int my_rank)
         /* For netCDF-4 files, set the chunksize to improve performance. */
         if (flavor[fmt] == PIO_IOTYPE_NETCDF4C || flavor[fmt] == PIO_IOTYPE_NETCDF4P)
         {
+            unsigned long long too_big_chunksize[NDIM] = {(unsigned long long)NC_MAX_INT64 + 42, X_DIM_LEN/2, Y_DIM_LEN/2};
+            if (PIOc_def_var_chunking(ncid, 0, NC_CHUNKED, too_big_chunksize) == PIO_NOERR)
+                ERR(ret);
+            
             printf("%d Defining chunksizes\n", my_rank);
             if ((ret = PIOc_def_var_chunking(ncid, 0, NC_CHUNKED, chunksize)))
                 ERR(ret);
