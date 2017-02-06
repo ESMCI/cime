1de138009de75d0e9f148e5b257f06179dfa59b8
edhartnett <edwardjameshartnett@gmail.com>
2017-02-06 11:55:37 -0700

    some fill value tests

--- tests/cunit/test_shared.c
+++ tests/cunit/test_shared.c
@@ -175,6 +175,7 @@ int run_test_main(int argc, char **argv, int min_ntasks, int max_ntasks,
         /* Figure out iotypes. */
         if ((ret = get_iotypes(&num_flavors, flavor)))
             ERR(ret);
+        printf("Runnings tests for %d flavors\n", num_flavors);
 
         /* Run tests without async feature. */
         if ((ret = test_no_async2(my_rank, num_flavors, flavor, test_comm, max_ntasks,
