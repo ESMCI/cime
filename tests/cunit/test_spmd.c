aa4af27eda945063a681c570e24e209c0bf6fef2
edhartnett <edwardjameshartnett@gmail.com>
2017-03-07 08:17:10 -0700

    added tests for cmp_rearr_opts()

--- tests/cunit/test_spmd.c
+++ tests/cunit/test_spmd.c
@@ -323,7 +323,63 @@ int test_rearranger_opts1()
 }
 
 /* Test some of the rearranger utility functions. */
-int test_rearranger_opts2()
+int test_cmp_rearr_opts()
+{
+    rearr_opt_t ro1;
+    rearr_opt_t ro2;
+
+    ro1.comm_type = PIO_REARR_COMM_P2P;
+    ro2.comm_type = PIO_REARR_COMM_P2P;
+
+    ro1.fcd = PIO_REARR_COMM_FC_2D_ENABLE;
+    ro2.fcd = PIO_REARR_COMM_FC_2D_ENABLE;
+
+    ro1.comm_fc_opts_comp2io.enable_hs = 0;
+    ro1.comm_fc_opts_comp2io.enable_isend = 0;
+    ro1.comm_fc_opts_comp2io.max_pend_req = 0;
+
+    ro1.comm_fc_opts_io2comp.enable_hs = 0;
+    ro1.comm_fc_opts_io2comp.enable_isend = 0;
+    ro1.comm_fc_opts_io2comp.max_pend_req = 0;
+    
+    ro2.comm_fc_opts_comp2io.enable_hs = 0;
+    ro2.comm_fc_opts_comp2io.enable_isend = 0;
+    ro2.comm_fc_opts_comp2io.max_pend_req = 0;
+
+    ro2.comm_fc_opts_io2comp.enable_hs = 0;
+    ro2.comm_fc_opts_io2comp.enable_isend = 0;
+    ro2.comm_fc_opts_io2comp.max_pend_req = 0;
+
+    /* They are equal. */
+    if (!cmp_rearr_opts(&ro1, &ro2))
+        return ERR_WRONG;
+
+    /* Change something. */
+    ro1.comm_type = PIO_REARR_COMM_COLL;
+
+    /* They are not equal. */
+    if (cmp_rearr_opts(&ro1, &ro2))
+        return ERR_WRONG;
+
+    /* Change something. */
+    ro2.comm_type = PIO_REARR_COMM_COLL;
+    ro1.fcd = PIO_REARR_COMM_FC_2D_DISABLE;
+    
+    /* They are not equal. */
+    if (cmp_rearr_opts(&ro1, &ro2))
+        return ERR_WRONG;
+
+    ro2.fcd = PIO_REARR_COMM_FC_2D_DISABLE;
+
+    /* They are equal again. */
+    if (!cmp_rearr_opts(&ro1, &ro2))
+        return ERR_WRONG;
+    
+    return 0;
+}
+
+/* Test some of the rearranger utility functions. */
+int test_rearranger_opts3()
 {
     iosystem_desc_t my_ios;
     iosystem_desc_t *ios = &my_ios;
@@ -510,45 +566,49 @@ int main(int argc, char **argv)
      * nothing. */
     if (my_rank < TARGET_NTASKS)
     {
-        printf("%d running tests for functions in pioc_sc.c\n", my_rank);
-        if ((ret = run_sc_tests(test_comm)))
-            return ret;
+        /* printf("%d running tests for functions in pioc_sc.c\n", my_rank); */
+        /* if ((ret = run_sc_tests(test_comm))) */
+        /*     return ret; */
 
-        printf("%d running spmd test code\n", my_rank);
-        if ((ret = run_spmd_tests(test_comm)))
-            return ret;
+        /* printf("%d running spmd test code\n", my_rank); */
+        /* if ((ret = run_spmd_tests(test_comm))) */
+        /*     return ret; */
         
-        printf("%d running CalcStartandCount test code\n", my_rank);
-        if ((ret = test_CalcStartandCount()))
+        /* printf("%d running CalcStartandCount test code\n", my_rank); */
+        /* if ((ret = test_CalcStartandCount())) */
+        /*     return ret; */
+
+        /* printf("%d running list tests\n", my_rank); */
+        /* if ((ret = test_lists())) */
+        /*     return ret; */
+
+        /* printf("%d running rearranger opts tests 1\n", my_rank); */
+        /* if ((ret = test_rearranger_opts1())) */
+        /*     return ret; */
+
+        printf("%d running tests for cmp_rearr_opts()\n", my_rank);
+        if ((ret = test_cmp_rearr_opts()))
             return ret;
 
-        printf("%d running list tests\n", my_rank);
-        if ((ret = test_lists()))
-            return ret;
+        /* printf("%d running rearranger opts tests 3\n", my_rank); */
+        /* if ((ret = test_rearranger_opts3())) */
+        /*     return ret; */
 
-        printf("%d running rearranger opts tests 1\n", my_rank);
-        if ((ret = test_rearranger_opts1()))
-            return ret;
+        /* printf("%d running compare_offsets tests\n", my_rank); */
+        /* if ((ret = test_compare_offsets())) */
+        /*     return ret; */
 
-        printf("%d running rearranger opts tests 2\n", my_rank);
-        if ((ret = test_rearranger_opts2()))
-            return ret;
+        /* printf("%d running ceil2/pair tests\n", my_rank); */
+        /* if ((ret = test_ceil2_pair())) */
+        /*     return ret; */
 
-        printf("%d running compare_offsets tests\n", my_rank);
-        if ((ret = test_compare_offsets()))
-            return ret;
+        /* printf("%d running find_mpi_type tests\n", my_rank); */
+        /* if ((ret = test_find_mpi_type())) */
+        /*     return ret; */
 
-        printf("%d running ceil2/pair tests\n", my_rank);
-        if ((ret = test_ceil2_pair()))
-            return ret;
-
-        printf("%d running find_mpi_type tests\n", my_rank);
-        if ((ret = test_find_mpi_type()))
-            return ret;
-
-        printf("%d running misc tests\n", my_rank);
-        if ((ret = test_misc()))
-            return ret;
+        /* printf("%d running misc tests\n", my_rank); */
+        /* if ((ret = test_misc())) */
+        /*     return ret; */
 
     } /* endif my_rank < TARGET_NTASKS */
 
