68daea6d5daf681b43443b25c50797b1832e0a43
edhartnett <edwardjameshartnett@gmail.com>
2017-03-16 17:18:18 -0600

    turned off async test

--- tests/cunit/test_pioc.c
+++ tests/cunit/test_pioc.c
@@ -1857,8 +1857,9 @@ int test_all(int iosysid, int num_flavors, int *flavor, int my_rank, MPI_Comm te
             return ret;
 
     /* This is a simple test that just writes the decomp. */
-    if ((ret = test_decomp_public_2(my_test_size, my_rank, iosysid, DIM_LEN, test_comm, async)))
-        return ret;
+    if (!async)
+        if ((ret = test_decomp_public_2(my_test_size, my_rank, iosysid, DIM_LEN, test_comm, async)))
+            return ret;
 
     /* Decompose the data over the tasks. */
     if (!async)
