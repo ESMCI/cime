e84873c543a3c37bcf2562be45c4b40fd1d7ecf7
edhartnett <edwardjameshartnett@gmail.com>
2017-04-02 09:10:24 -0600

    more assert

--- src/clib/pio_rearrange.c
+++ src/clib/pio_rearrange.c
@@ -1100,7 +1100,8 @@ int determine_fill(iosystem_desc_t *ios, io_desc_t *iodesc, const int *gdimlen,
     int mpierr; /* Return code from MPI calls. */
 
     /* Check inputs. */
-    pioassert(ios && iodesc, "invalid input", __FILE__, __LINE__);
+    pioassert(ios && iodesc && gdimlen, "invalid input",
+              __FILE__, __LINE__);
 
     /* Determine size of data space. */
     for (int i = 0; i < iodesc->ndims; i++)
