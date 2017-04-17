8a571ef14c2569bd7e46feb10474f045be29156c
Jim Edwards <jedwards@ucar.edu>
2017-04-17 12:53:54 -0600

    need to specify queue

--- ctest/runctest-nwscla.sh
+++ ctest/runctest-nwscla.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #==============================================================================
 #
-#  This script defines how to run CTest on the NCAR Wyoming Supercomputing 
-#  Center systems (yellowstone/caldera/geyser).
+#  This script defines how to run CTest on the NCAR Wyoming Supercomputing
+#  Center systems (cheyenne/laramie).
 #
 #  This assumes the CTest model name (e.g., "Nightly") is passed to it when
 #  run.
@@ -19,7 +19,8 @@ model=$2
 echo "#!/bin/sh" > runctest.sh
 echo "#PBS -l walltime=01:00:00" >> runctest.sh
 echo "#PBS -l select=1:ncpus=8:mpiprocs=8" >> runctest.sh
-echo "#PBS -A SCSG0002" >> runctest.sh
+echo "#PBS -A P93300606" >> runctest.sh
+echo "#PBS -q regular" >> runctest.sh
 echo "export PIO_DASHBOARD_SITE=nwscla-${HOSTNAME}" >> runctest.sh
 echo "CTESTCMD=`which ctest`" >> runctest.sh
 echo "\$CTESTCMD -S ${scrdir}/CTestScript-Test.cmake,${model} -V" >> runctest.sh
@@ -28,7 +29,7 @@ echo "\$CTESTCMD -S ${scrdir}/CTestScript-Test.cmake,${model} -V" >> runctest.sh
 chmod +x runctest.sh
 
 # Submit the job to the queue
-jobid=`qsub runctest.sh`
+jobid=`qsub -l walltime=01:00:00 runctest.sh`
 
 # Wait for the job to complete before exiting
 while true; do
@@ -42,4 +43,3 @@ while true; do
 done
 
 exit 0
-
