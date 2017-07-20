1c1c1721b2b2b0c4d787a328c96437a933f404eb
Alice Bertini <aliceb@ucar.edu>
2017-07-19 23:14:08 -0600

    update html for PR #1752

--- _static/bizstyle.js
+++ _static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("CIME 5.3.0_alpha17 documentation");
+        $("li.nav-item-0 a").text("CIME 5.3.0 documentation");
     }
 });
