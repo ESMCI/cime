a51e5b2eb2206cbfc22933d0e7dc170a55985218
Alok Kumar Gupta <agu002@uni.no>
2019-04-30 11:11:01 +0200

    modifying noresm_l_archive.bash

--- scripts/Tools/noresm_l_archive.bash
+++ scripts/Tools/noresm_l_archive.bash
@@ -7,10 +7,11 @@ export DOUT_L_ROOT=`./xmlquery DOUT_L_ROOT --value`
 export DOUT_S_ROOT=`./xmlquery DOUT_S_ROOT --value`
 export CASE=`./xmlquery CASE --value`
 export CASEROOT=`./xmlquery CASEROOT --value`
-echo $DOUT_L_HOSTNAME
-echo $DOUT_L_ROOT
-echo $DOUT_S_ROOT
-echo $CASEROOT
+#echo $DOUT_L_HOSTNAME
+#echo $DOUT_L_ROOT
+#echo $DOUT_S_ROOT
+#echo $CASEROOT
+#echo $USER
 #-----------------------------------------------------------------------
 # Create remote archive root directory.
 #-----------------------------------------------------------------------
@@ -21,7 +22,7 @@ ssh $USER@$DOUT_L_HOSTNAME "mkdir -p $DOUT_L_ROOT"
 #-----------------------------------------------------------------------
 export LID="`date +%y%m%d-%H%M%S`"
 echo "$sn: Archive model output and restart files..."
-  rsync -avh --progress $DOUT_S_ROOT $USER@$DOUT_L_HOSTNAME:$DOUT_L_ROOT &> $DOUT_S_ROOT/../$CASE.la.log.$LID 
+  rsync -avh --progress $DOUT_S_ROOT $USER@$DOUT_L_HOSTNAME:$DOUT_L_ROOT &> $DOUT_S_ROOT/../$CASE.la.log.$LID &
 
 echo "$sn: Archiving model output completed."
 echo "$sn: Archiving CASE directory..."
