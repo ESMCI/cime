259622303d463acc9ed8bd3aa1a0bc453e832f04
Ed Hartnett <edwardjameshartnett@gmail.com>
2019-07-08 06:29:36 -0600

    developing fortran test of ncint

--- tests/fncint/ftst_pio.f90
+++ tests/fncint/ftst_pio.f90
@@ -7,12 +7,18 @@ program ftst_pio
   include 'mpif.h'
 
   integer :: myRank, ntasks
+  type(iosystem_desc_t) :: ioSystem
+  integer :: niotasks = 1, numAggregator = 0, stride = 1, base = 0
   integer :: ierr
 
   call MPI_Init(ierr)
   call MPI_Comm_rank(MPI_COMM_WORLD, myRank, ierr)
   call MPI_Comm_size(MPI_COMM_WORLD, ntasks, ierr)
 
+  call PIO_init(myRank, MPI_COMM_WORLD, niotasks, numAggregator, &
+       stride, PIO_rearr_subset, ioSystem, base)
+
+  call PIO_finalize(ioSystem, ierr)
   call MPI_Finalize(ierr)
   if (myRank .eq. 0) then
      print *, '*** SUCCESS running ftst_pio!'
