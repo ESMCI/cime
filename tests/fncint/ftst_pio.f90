e03d43f8699c1d25bda9a6397dd97a39a77013ae
Ed Hartnett <edwardjameshartnett@gmail.com>
2019-07-08 06:07:16 -0600

    adding run_tests.sh script in fncint test directory

--- tests/fncint/ftst_pio.f90
+++ tests/fncint/ftst_pio.f90
@@ -4,6 +4,17 @@
 program ftst_pio
   use pio
   implicit none
+  include 'mpif.h'
 
-  print *, '*** SUCCESS running ftst_pio!'
+  integer :: myRank, ntasks
+  integer :: ierr
+
+  call MPI_Init(ierr)
+  call MPI_Comm_rank(MPI_COMM_WORLD, myRank, ierr)
+  call MPI_Comm_size(MPI_COMM_WORLD, ntasks, ierr)
+
+  call MPI_Finalize(ierr)
+  if (myRank .eq. 0) then
+     print *, '*** SUCCESS running ftst_pio!'
+  endif
 end program ftst_pio
