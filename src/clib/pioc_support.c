9f91675e5057a1dfd0f27c5fbe8fa028c25794c0
edwardhartnett <edwardjameshartnett@gmail.com>
2019-08-23 13:41:01 -0600

    added contents of ncidp when ncidp_present to the parameters sent when a file is created in async mode

--- src/clib/pioc_support.c
+++ src/clib/pioc_support.c
@@ -2019,6 +2019,9 @@ PIOc_createfile_int(int iosysid, int *ncidp, int *iotype, const char *filename,
                 mpierr = MPI_Bcast(&use_ext_ncid, 1, MPI_INT, ios->compmaster, ios->intercomm);
             if (!mpierr)
                 mpierr = MPI_Bcast(&ncidp_present, 1, MPI_CHAR, ios->compmaster, ios->intercomm);
+            if (ncidp_present)
+                if (!mpierr)
+                    mpierr = MPI_Bcast(ncidp, 1, MPI_INT, ios->compmaster, ios->intercomm);
             PLOG((2, "len %d filename %s iotype %d mode %d use_ext_ncid %d "
                   "ncidp_present %d", len, filename, file->iotype, mode,
                   use_ext_ncid, ncidp_present));
