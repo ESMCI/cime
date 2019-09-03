20a3b29cc4037aee1dd1427c824a04be666d73c8
edwardhartnett <edwardjameshartnett@gmail.com>
2019-09-03 11:25:50 -0600

    getting close to turning on int reads

--- tests/ncint/tst_pio_async.c
+++ tests/ncint/tst_pio_async.c
@@ -96,19 +96,19 @@ main(int argc, char **argv)
             /* Open the file. */
             if (nc_open(FILE_NAME, NC_UDF0, &ncid)) PERR;
 
-            /* /\* Read distributed arrays. *\/ */
-            /* if (!(data_in = malloc(elements_per_pe * sizeof(int)))) PERR; */
+            /* Read distributed arrays. */
+            if (!(data_in = malloc(elements_per_pe * sizeof(int)))) PERR;
             /* if (nc_get_vard_int(ncid, varid, ioid, 0, data_in)) PERR; */
 
-            /* /\* Check results. *\/ */
+            /* Check results. */
             /* for (i = 0; i < elements_per_pe; i++) */
             /*     if (data_in[i] != my_data[i]) PERR; */
 
             /* Close file. */
             if (nc_close(ncid)) PERR;
 
-            /* /\* Free resources. *\/ */
-            /* free(data_in); */
+            /* Free resources. */
+            free(data_in);
             free(my_data);
             if (nc_free_decomp(ioid)) PERR;
             if (nc_free_iosystem(iosysid)) PERR;
