b16e033d3be7a11170c1c5b32e604427484a0570
Edward Hartnett <edward.hartnett@noaa.gov>
2019-12-03 12:09:33 -0700

    testing different iotypes in performance test

--- tests/ncint/tst_ncint_perf.c
+++ tests/ncint/tst_ncint_perf.c
@@ -24,6 +24,7 @@
 #define NDIM2 2
 #define NDIM3 3
 #define NUM_TIMESTEPS 1
+#define NUM_MODES 2
 
 extern NC_Dispatch NCINT_dispatcher;
 
@@ -46,7 +47,7 @@ main(int argc, char **argv)
     if (!my_rank)
         printf("\n*** Testing netCDF integration PIO performance.\n");
     if (!my_rank)
-        printf("*** testing simple async use of netCDF integration layer...");
+        printf("*** testing simple async use of netCDF integration layer...\n");
     {
         int ncid, ioid;
         int dimid[NDIM3], varid;
@@ -86,7 +87,7 @@ main(int argc, char **argv)
                          num_procs2, NULL, NULL, NULL, PIO_REARR_BOX, &iosysid))
             PERR;
 
-        if (my_rank > num_io_procs)
+        if (my_rank >= num_io_procs)
         {
             struct timeval starttime, endtime;
             long long startt, endt;
@@ -94,58 +95,65 @@ main(int argc, char **argv)
             float num_megabytes = DIM_LEN_X * DIM_LEN_Y * sizeof(int) / (float)1000000 * NUM_TIMESTEPS;
             float delta_in_sec;
             float mb_per_sec;
-            int t;
+            int cmode[NUM_MODES] = {NC_PIO, NC_PIO|NC_NETCDF4};
+            char mode_name[NUM_MODES][NC_MAX_NAME + 1] = {"netCDF-classic sequential",
+                                                          "netCDF-4/HDF5 sequential"};
+            int t, m;
 
-            /* Create a file with a 3D record var. */
-            if (nc_create(FILE_NAME, NC_PIO|NC_NETCDF4, &ncid)) PERR;
-            if (nc_def_dim(ncid, DIM_NAME_UNLIMITED, dimlen[0], &dimid[0])) PERR;
-            if (nc_def_dim(ncid, DIM_NAME_X, dimlen[1], &dimid[1])) PERR;
-            if (nc_def_dim(ncid, DIM_NAME_Y, dimlen[2], &dimid[2])) PERR;
-            if (nc_def_var(ncid, VAR_NAME, NC_INT, NDIM3, dimid, &varid)) PERR;
-            if (nc_enddef(ncid)) PERR;
-
-            /* Calculate a decomposition for distributed arrays. */
-            elements_per_pe = DIM_LEN_X * DIM_LEN_Y / (ntasks - num_io_procs);
-            /* printf("my_rank %d elements_per_pe %ld\n", my_rank, elements_per_pe); */
-
-            if (!(compdof = malloc(elements_per_pe * sizeof(size_t))))
-                PERR;
-            for (i = 0; i < elements_per_pe; i++)
+            for (m = 0; m < NUM_MODES; m++)
             {
-                compdof[i] = (my_rank - num_io_procs) * elements_per_pe + i;
-                /* printf("my_rank %d compdof[%d]=%ld\n", my_rank, i, compdof[i]); */
-            }
+                /* Create a file with a 3D record var. */
+                if (nc_create(FILE_NAME, cmode[m], &ncid)) PERR;
+                if (nc_def_dim(ncid, DIM_NAME_UNLIMITED, dimlen[0], &dimid[0])) PERR;
+                if (nc_def_dim(ncid, DIM_NAME_X, dimlen[1], &dimid[1])) PERR;
+                if (nc_def_dim(ncid, DIM_NAME_Y, dimlen[2], &dimid[2])) PERR;
+                if (nc_def_var(ncid, VAR_NAME, NC_INT, NDIM3, dimid, &varid)) PERR;
+                if (nc_enddef(ncid)) PERR;
 
-            /* Create the PIO decomposition for this test. */
-            if (nc_def_decomp(iosysid, PIO_INT, NDIM2, &dimlen[1], elements_per_pe,
-                              compdof, &ioid, 1, NULL, NULL)) PERR;
-            free(compdof);
+                /* Calculate a decomposition for distributed arrays. */
+                elements_per_pe = DIM_LEN_X * DIM_LEN_Y / (ntasks - num_io_procs);
+                /* printf("my_rank %d elements_per_pe %ld\n", my_rank, elements_per_pe); */
 
-            /* Create some data on this processor. */
-            if (!(my_data = malloc(elements_per_pe * sizeof(int)))) PERR;
-            for (i = 0; i < elements_per_pe; i++)
-                my_data[i] = my_rank * 10 + i;
+                if (!(compdof = malloc(elements_per_pe * sizeof(size_t))))
+                    PERR;
+                for (i = 0; i < elements_per_pe; i++)
+                {
+                    compdof[i] = (my_rank - num_io_procs) * elements_per_pe + i;
+                    /* printf("my_rank %d compdof[%d]=%ld\n", my_rank, i, compdof[i]); */
+                }
 
-            /* Start the clock. */
-            gettimeofday(&starttime, NULL);
+                /* Create the PIO decomposition for this test. */
+                if (nc_def_decomp(iosysid, PIO_INT, NDIM2, &dimlen[1], elements_per_pe,
+                                  compdof, &ioid, 1, NULL, NULL)) PERR;
+                free(compdof);
 
-            /* Write some data with distributed arrays. */
-            for (t = 0; t < NUM_TIMESTEPS; t++)
-                if (nc_put_vard_int(ncid, varid, ioid, t, my_data)) PERR;
-            if (nc_close(ncid)) PERR;
+                /* Create some data on this processor. */
+                if (!(my_data = malloc(elements_per_pe * sizeof(int)))) PERR;
+                for (i = 0; i < elements_per_pe; i++)
+                    my_data[i] = my_rank * 10 + i;
 
-            /* Stop the clock. */
-            gettimeofday(&endtime, NULL);
+                /* Start the clock. */
+                gettimeofday(&starttime, NULL);
 
-            /* Compute the time delta */
-            startt = (1000000 * starttime.tv_sec) + starttime.tv_usec;
-            endt = (1000000 * endtime.tv_sec) + endtime.tv_usec;
-            delta = (endt - startt)/NUM_TIMESTEPS;
-            delta_in_sec = (float)delta / 1000000;
-            mb_per_sec = num_megabytes / delta_in_sec;
-            if (my_rank == num_io_procs)
-                printf("\n%d\t%d\t%d\t%d\t%d\t%8.3f\t%8.1f\t%8.3f\n", ntasks, num_io_procs,
-                       1, 0, 1, delta_in_sec, num_megabytes, mb_per_sec);
+                /* Write some data with distributed arrays. */
+                for (t = 0; t < NUM_TIMESTEPS; t++)
+                    if (nc_put_vard_int(ncid, varid, ioid, t, my_data)) PERR;
+                if (nc_close(ncid)) PERR;
+
+                /* Stop the clock. */
+                gettimeofday(&endtime, NULL);
+
+                /* Compute the time delta */
+                startt = (1000000 * starttime.tv_sec) + starttime.tv_usec;
+                endt = (1000000 * endtime.tv_sec) + endtime.tv_usec;
+                delta = (endt - startt)/NUM_TIMESTEPS;
+                delta_in_sec = (float)delta / 1000000;
+                mb_per_sec = num_megabytes / delta_in_sec;
+                if (my_rank == num_io_procs)
+                    printf("%s\t%d\t%d\t%d\t%d\t%d\t%8.3f\t%8.1f\t%8.3f\n",
+                           mode_name[m], ntasks, num_io_procs, 1, 0, 1,
+                           delta_in_sec, num_megabytes, mb_per_sec);
+            } /* next mode flag */
 
             free(my_data);
             if (nc_free_decomp(ioid)) PERR;
