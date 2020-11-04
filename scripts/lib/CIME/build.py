c289f162d79654550fe6d551bfc5dc1bdb70dadd
Bill Sacks <sacks@ucar.edu>
2020-11-04 12:19:56 -0700

    Avoid building CDEPS with CTSM's LILAC
    For now, avoid building CDEPS with CTSM's LILAC because it's not needed
    in this configuration and CDEPS relies on unreleased ESMF
    code. Eventually we will require CDEPS (for its streams functionality),
    at which point CDEPS should only require released ESMF code; then we
    should revert the changes in this commit.

--- scripts/lib/CIME/build.py
+++ scripts/lib/CIME/build.py
@@ -339,6 +339,7 @@ def _build_libraries(case, exeroot, sharedpath, caseroot, cimeroot, libroot, lid
             os.makedirs(shared_item)
 
     mpilib = case.get_value("MPILIB")
+    lilac_mode = case.get_value("LILAC_MODE")
     ufs_driver = os.environ.get("UFS_DRIVER")
     if ufs_driver:
         logger.info("UFS_DRIVER is set to {}".format(ufs_driver))
@@ -355,7 +356,13 @@ def _build_libraries(case, exeroot, sharedpath, caseroot, cimeroot, libroot, lid
 
     # Build shared code of CDEPS nuopc data models
     cdeps_build_script = None
-    if comp_interface == "nuopc" and (not ufs_driver or ufs_driver != 'nems'):
+    if (comp_interface == "nuopc" and (not ufs_driver or ufs_driver != 'nems')
+        and lilac_mode != 'on'):
+        # For now, we avoid building CDEPS with CTSM's LILAC because it's not needed in
+        # this configuration and CDEPS relies on unreleased ESMF code. Eventually we will
+        # require CDEPS (for its streams functionality), at which point CDEPS should only
+        # require released ESMF code; then we should remove the 'lilac_mode' part of this
+        # conditional.
         libs.append("CDEPS")
         cdeps_build_script = os.path.join(srcroot, "components", "cdeps", "cime_config", "buildlib")
 
