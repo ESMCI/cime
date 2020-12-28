2eba7bbbaee47b05dd683312bfb5f3e30125f95c
alperaltuntas <alperaltuntas@gmail.com>
2020-12-27 21:10:18 -0700

    Expand variables in values and eval formulas

--- CIME/ParamGen/paramgen.py
+++ CIME/ParamGen/paramgen.py
@@ -80,14 +80,21 @@ class ParamGen(ABC):
         assert isinstance(expr, str), "Expression passed to _expand_vars must be string."
         expandable_vars = re.findall(r'(\$\w+|\${\w+\})',expr)
         for word in expandable_vars:
-            word_stripped = word.\
+            word_stripped = word.strip().\
                 replace("$","").\
                 replace("{","").\
                 replace("}","")
             word_expanded = expand_func(word_stripped)
-            assert word_expanded!=None, "Cannot expand the variable "+word+" using the given expand_func."
-            assert isinstance(word_expanded,str), "expand_func must return string."
+            assert word_expanded!=None, "Cannot determine the value of the variable: "+word+"."
+
+            # enclose with quotes if expanded var is a string and is expression without curly braces
+            if isinstance(word_expanded, str) and word[1]!='{':
+                word_expanded = '"'+word_expanded+'"'
+            else:
+                word_expanded = str(word_expanded)
+
             expr = expr.replace(word,word_expanded)
+
         return expr
 
 
@@ -101,8 +108,8 @@ class ParamGen(ABC):
         if all(keys_logical):
             return True
         elif any(keys_logical):
-            print("data_dict: ", data_dict)
-            raise RuntimeError("Only subset of the keys of the above dict are guards, i.e., logical expressions")
+            raise RuntimeError("Only subset of the following are guarded entries, i.e., logical "+
+                                "expressions as keys:\n\t"+str(data_dict))
         else:
             return False
 
@@ -133,7 +140,7 @@ class ParamGen(ABC):
             if len(guards_eval_true)>1 and "else" in guards_eval_true:
                 guards_eval_true.remove("else")
             elif len(guards_eval_true)==0:
-                raise RuntimeError("None of the guards evaluate to true: "+str(data_dict))
+                return None
 
             if self._match == 'first':
                 return data_dict[guards_eval_true[0]]
@@ -158,7 +165,15 @@ class ParamGen(ABC):
             data_dict = self._impose_guards(data_dict)
 
         # Continue reducing the data if it is still a dictionary (nested or not).
-        if isinstance(data_dict, dict):
+        if isinstance(data_dict, str):
+            # (3) Expand variables in values, .e.g, "$OCN_GRID" to "gx1v7":
+            data_dict = ParamGen._expand_vars(data_dict, expand_func)
+
+            # (4) Evaluate the formulas as values, e.g., "= 3+4", "= [i for i in range(5)]", etc.
+            if is_formula(data_dict):
+                data_dict = eval_formula(data_dict.strip()[1:])
+
+        elif isinstance(data_dict, dict):
 
             # (3) Expand variables in values, .e.g, "$OCN_GRID" to "gx1v7":
             for key, val in data_dict.copy().items():
@@ -193,5 +208,4 @@ class ParamGen(ABC):
         assert len(self._data)>0, "Empty ParamGen data."
 
         self._data = self._reduce_recursive(self._data, expand_func)
-
         self._reduced = True
