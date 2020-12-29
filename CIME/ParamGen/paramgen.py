76d64a6b5d226c9b17e4033f2bd0e24ab1681fc4
alperaltuntas <alperaltuntas@gmail.com>
2020-12-28 22:04:10 -0700

    minor enhancements and corrections

--- CIME/ParamGen/paramgen.py
+++ CIME/ParamGen/paramgen.py
@@ -15,6 +15,8 @@ class ParamGen(ABC):
     ----------
     data : dict or OrderedDict
         The data attribute to operate over. See Methods for the list of operations.
+    match: str
+        "first" or "last".
     Methods
     -------
     from_json(input_path)
@@ -27,6 +29,7 @@ class ParamGen(ABC):
         assert isinstance(data_dict, dict), \
             "ParamGen class requires a dict or OrderedDict as the initial data."
         #self._validate_schema(data_dict)
+        self._original_data = deepcopy(data_dict)
         self._data = deepcopy(data_dict)
         self._reduced = False
         self._match = match
@@ -36,14 +39,26 @@ class ParamGen(ABC):
         """Returns the data attribute of the ParamGen instance."""
         return self._data
 
+    @property
+    def reduced(self):
+        """Returns True if reduce() method is called for this instance of ParamGen."""
+        return self._reduced
+
+    @property
+    def is_empty(self):
+        """Returns True if the data property is empty."""
+        return len(self._data)==0
+
     @classmethod
-    def from_json(cls, input_path):
+    def from_json(cls, input_path, match='last'):
         """
         Reads in a given json input file and initializes a ParamGen object.
         Parameters
         ----------
         input_path: str
             Path to json input file containing the defaults.
+        match: str
+            "first" or "last"
         Returns
         -------
         ParamGen
@@ -52,17 +67,19 @@ class ParamGen(ABC):
 
         import json
         with open(input_path) as json_file:
-            _data = json.load(json_file, object_pairs_hook=OrderedDict)
-        return cls(_data)
+            _data = json.load(json_file)
+        return cls(_data, match)
 
     @classmethod
-    def from_yaml(cls, input_path):
+    def from_yaml(cls, input_path, match='last'):
         """
         Reads in a given yaml input file and initializes a ParamGen object.
         Parameters
         ----------
         input_path: str
             Path to yaml input file containing the defaults.
+        match: str
+            "first" or "last"
         Returns
         -------
         ParamGen
@@ -72,11 +89,15 @@ class ParamGen(ABC):
         import yaml
         with open(input_path) as yaml_file:
             _data = yaml.safe_load(yaml_file)
-        return cls(_data)
+        return cls(_data, match)
 
 
     @staticmethod
     def _expand_vars(expr, expand_func):
+
+        if expand_func==None:
+            return expr # No expansion function is provided, so return.
+
         assert isinstance(expr, str), "Expression passed to _expand_vars must be string."
         expandable_vars = re.findall(r'(\$\w+|\${\w+\})',expr)
         for word in expandable_vars:
@@ -87,7 +108,7 @@ class ParamGen(ABC):
             word_expanded = expand_func(word_stripped)
             assert word_expanded!=None, "Cannot determine the value of the variable: "+word+"."
 
-            # enclose with quotes if expanded var is a string and is expression without curly braces
+            # enclose with quotes if expanded var is a string and is expression sans curly braces
             if isinstance(word_expanded, str) and word[1]!='{':
                 word_expanded = '"'+word_expanded+'"'
             else:
@@ -154,18 +175,22 @@ class ParamGen(ABC):
 
         # (1) Expand variables in keys, .e.g, "$OCN_GRID" to "gx1v7":
         if expand_func!=None:
-            for key in data_dict.copy():
+            new_data_dict  = {}
+            for key in data_dict:
+                new_key = key
                 if has_expandable_var(key):
                     new_key = ParamGen._expand_vars(key, expand_func)
-                    data_dict[new_key] = data_dict.pop(key)
+                new_data_dict[new_key] = data_dict[key]
+            data_dict = new_data_dict
 
         # (2) Evaluate the keys if they are all logical expressions, i.e., guards.
         # Pick the value of the first or last key evaluating to True and drop everything else.
         while ParamGen._is_guarded_dict(data_dict):
             data_dict = self._impose_guards(data_dict)
 
-        # Continue reducing the data if it is still a dictionary (nested or not).
+        # If the data_dict is reduced to a string, expand vars and eval formulas as a last step.
         if isinstance(data_dict, str):
+
             # (3) Expand variables in values, .e.g, "$OCN_GRID" to "gx1v7":
             data_dict = ParamGen._expand_vars(data_dict, expand_func)
 
@@ -173,6 +198,7 @@ class ParamGen(ABC):
             if is_formula(data_dict):
                 data_dict = eval_formula(data_dict.strip()[1:])
 
+        # Continue reducing the data if it is still a dictionary (nested or not).
         elif isinstance(data_dict, dict):
 
             # (3) Expand variables in values, .e.g, "$OCN_GRID" to "gx1v7":
@@ -204,8 +230,8 @@ class ParamGen(ABC):
         """
 
         assert callable(expand_func) or expand_func==None, "expand_func argument must be a function"
-        assert not self._reduced, "ParamGen data already reduced."
-        assert len(self._data)>0, "Empty ParamGen data."
+        assert not self.reduced, "ParamGen data already reduced."
+        assert not self.is_empty, "Empty ParamGen data."
 
         self._data = self._reduce_recursive(self._data, expand_func)
         self._reduced = True
@@ -216,7 +242,7 @@ class ParamGen(ABC):
         """
 
         assert isinstance(pg,ParamGen), "can only append ParamGen to Paramgen"
-        assert self._reduced == pg._reduced, "Cannot append reduced ParamGen instance to unreduced, and vice versa."
+        assert self.reduced == pg.reduced, "Cannot append reduced ParamGen instance to unreduced, and vice versa."
 
         def _append_recursive(old_dict, new_dict):
             for key, val in new_dict.items():
@@ -230,3 +256,7 @@ class ParamGen(ABC):
                     old_dict[key] = new_dict[key]
 
         _append_recursive(self._data, pg._data)
+
+    def reset(self):
+        self._data = deepcopy(self._original_data)
+        self._reduced = False
