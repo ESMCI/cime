188d73eb804a2d410eafb08b582e24ebab7fe848
Jian Sun <sunjian@ucar.edu>
2021-06-10 09:01:15 -0600

    Easier settings for set_device_rank.sh
    	modified:   scripts/lib/CIME/case/case_setup.py

--- scripts/lib/CIME/case/case_setup.py
+++ scripts/lib/CIME/case/case_setup.py
@@ -299,19 +299,19 @@ def case_setup(self, clean=False, test_mode=False, reset=False, keep=None):
                                 is_batch=is_batch)
  
     # put the following section here to make sure the rundir is generated first
-    if is_batch:
-        machdir = self.get_value("MACHDIR")
-        mach = self.get_value("MACH")
-        env_module = self.get_env("batch")
-        overrides = env_module.get_job_overrides(jobid, self)
-        input_template = os.path.join(machdir,"mpi_run_gpu.{}".format(mach))
-        if os.path.isfile(input_template):
-            # update the wrapper script that sets the device id for each MPI rank
-            output_text = transform_vars(open(input_template,"r").read(), case=self, overrides=overrides)
-    
-            # write it out to the run dir
-            rundir = self.get_value("RUNDIR")
-            output_name = os.path.join(rundir,"set_device_rank.sh")
-            logger.info("Creating file {}".format(output_name))
-            with open(output_name, "w") as f:
-                f.write(output_text)
+    machdir = self.get_value("MACHDIR")
+    mach = self.get_value("MACH")
+    ngpus_per_node = self.get_value("NGPUS_PER_NODE")
+    overrides = {}
+    overrides["ngpus_per_node"] = ngpus_per_node 
+    input_template = os.path.join(machdir,"mpi_run_gpu.{}".format(mach))
+    if os.path.isfile(input_template):
+        # update the wrapper script that sets the device id for each MPI rank
+        output_text = transform_vars(open(input_template,"r").read(), case=self, overrides=overrides)
+
+        # write it out to the run dir
+        rundir = self.get_value("RUNDIR")
+        output_name = os.path.join(rundir,"set_device_rank.sh")
+        logger.info("Creating file {}".format(output_name))
+        with open(output_name, "w") as f:
+            f.write(output_text)
