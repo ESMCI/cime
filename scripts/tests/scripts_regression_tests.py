828fd3227d8057beca04e951434ae49b27b67302
Jim Edwards <jedwards@ucar.edu>
2021-10-12 09:07:08 -0600

    hot fix for scripts regression tests

--- scripts/tests/scripts_regression_tests.py
+++ scripts/tests/scripts_regression_tests.py
@@ -2067,7 +2067,7 @@ class K_TestCimeCase(TestCreateTestCommon):
         if os.path.exists(testdir):
             shutil.rmtree(testdir)
         args = "--case {name} --script-root {testdir} --compset X --res f19_g16 --handle-preexisting-dirs=r --output-root {testdir}".format(name=testcase_name, testdir=testdir)
-        if CIME.utils.get_cime_default_driver() == 'nuopc':
+        if get_model() == "cesm":
             args += " --run-unsupported"
 
         run_cmd_assert_result(self, "{}/create_newcase {}".format(SCRIPT_DIR, args),
