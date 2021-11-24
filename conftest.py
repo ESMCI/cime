9d2fd57b397f2a875f55f7c92042e42f32a05287
Jason Boutte <boutte.jason@gmail.com>
2021-11-24 00:39:31 +0000

    Fixes adding missing functions

--- conftest.py
+++ conftest.py
@@ -1,5 +1,8 @@
+import atexit
 import os
 import sys
+import logging
+import functools
 
 CIMEROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "scripts", "lib"))
 sys.path.insert(0, CIMEROOT)
@@ -9,6 +12,33 @@ import pytest
 from CIME import utils
 from CIME.tests.base import BaseTestCase
 from CIME.XML.machines import Machines
+
+
+os.environ["CIME_GLOBAL_WALLTIME"] = "0:05:00"
+
+def write_provenance_info(machine, test_compiler, test_mpilib, test_root):
+    curr_commit = utils.get_current_commit(repo=CIMEROOT)
+    logging.info("\nTesting commit %s" % curr_commit)
+    cime_model = utils.get_model()
+    logging.info("Using cime_model = %s" % cime_model)
+    logging.info("Testing machine = %s" % machine)
+    if test_compiler is not None:
+        logging.info("Testing compiler = %s"% test_compiler)
+    if test_mpilib is not None:
+        logging.info("Testing mpilib = %s"% test_mpilib)
+    logging.info("Test root: %s" % test_root)
+    logging.info("Test driver: %s" % utils.get_cime_default_driver())
+    logging.info("Python version {}\n".format(sys.version))
+
+def cleanup(test_root):
+    if os.path.exists(test_root):
+        testreporter = os.path.join(test_root,"testreporter")
+        files = os.listdir(test_root)
+        if len(files)==1 and os.path.isfile(testreporter):
+            os.unlink(testreporter)
+        if not os.listdir(test_root):
+            print("All pass, removing directory:", test_root)
+            os.rmdir(test_root)
 
 def pytest_addoption(parser):
     parser.addoption("--fast", action="store_true",
@@ -108,3 +138,7 @@ def setup(pytestconfig):
                                  "scripts_regression_test.%s"% utils.get_timestamp())
 
     BaseTestCase.TEST_ROOT = TEST_ROOT
+
+    write_provenance_info(MACHINE, TEST_COMPILER, TEST_MPILIB, TEST_ROOT)
+
+    atexit.register(functools.partial(cleanup, TEST_ROOT))
