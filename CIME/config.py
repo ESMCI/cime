ab26bcc40edc21b128a548b9c3ed76598b893fff
Jason Boutte <boutte.jason@gmail.com>
2022-06-15 20:36:59 +0000

    Fixes Config class to be singleton

--- CIME/config.py
+++ CIME/config.py
@@ -8,6 +8,12 @@ logger = logging.getLogger(__name__)
 
 
 class Config:
+    def __new__(cls):
+        if not hasattr(cls, "_instance"):
+            cls._instance = super(Config, cls).__new__(cls)
+
+        return cls._instance
+
     def __init__(self):
         pass
 
