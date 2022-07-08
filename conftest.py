235d9eb18f2f58951cf86e32a0d35958784061e0
Jason Boutte <boutte.jason@gmail.com>
2022-07-08 20:23:21 +0000

    Fixes loading model customized values when running tests

--- conftest.py
+++ conftest.py
@@ -7,6 +7,7 @@ sys.path.insert(0, CIMEROOT)
 import pytest
 
 from CIME import utils
+from CIME.config import Config
 from CIME.tests import scripts_regression_tests
 
 os.environ["CIME_GLOBAL_WALLTIME"] = "0:05:00"
@@ -35,6 +36,13 @@ def pytest_configure(config):
 def setup(pytestconfig):
     # ensure we start from CIMEROOT for each module
     os.chdir(CIMEROOT)
+
+    srcroot = utils.get_src_root()
+
+    customize_path = os.path.join(srcroot, "cime_config", "customize")
+
+    if os.path.exists(customize_path):
+        Config.instance().load(customize_path)
 
     # ensure GLOABL is reset
     utils.GLOBAL = {}
