5ae7106d03f5841d9d43c4e4a0a8782c7bf0a98f
Jim Edwards <jedwards@ucar.edu>
2022-10-17 09:42:34 -0600

    remove debug print

--- CIME/case/case.py
+++ CIME/case/case.py
@@ -451,7 +451,6 @@ class Case(object):
 
     def get_value(self, item, attribute=None, resolved=True, subgroup=None):
         result = None
-        print(f"item is {item}")
         for env_file in self._files:
             # Wait and resolve in self rather than in env_file
             result = env_file.get_value(
