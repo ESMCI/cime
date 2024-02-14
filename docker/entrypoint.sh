7fc53df2b6cf760ae79d7fb46c6404592babca62
Jason Boutte <boutte.jason@gmail.com>
2024-02-14 15:02:50 -0800

    Fixes cloning e3sm if it exists, adds comments

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-if [[ -n "${DEBUG}" ]]
+DEBUG="${DEBUG:-false}"
+SRC_PATH="${SRC_PATH:-`pwd`}"
+# Treeless clone
+GIT_FLAGS="${GIT_FLAGS:---filter=tree:0}"
+# Shallow submodule checkout
+GIT_SUBMODULE_FLAGS="${GIT_SUBMODULE_FLAGS:---recommend-shallow}"
+
+echo "DEBUG = ${DEBUG}"
+echo "SRC_PATH = ${SRC_PATH}"
+echo "GIT_FLAGS = ${GIT_FLAGS}"
+echo "GIT_SUBMODULE_FLAGS = ${GIT_SUBMODULE_FLAGS}"
+
+if [[ "$(echo ${DEBUG} | tr -s '[:upper:]' '[:lower:]')" == "true" ]]
 then
     set -x
 fi
-
-SRC_PATH="`pwd`"
-GIT_FLAGS="--filter=tree:0"
-GIT_SUBMODULE_FLAGS="--recommend-shallow"
 
 #######################################
 # Fixes mct/mpeu to use ARFLAGS environment variable
@@ -44,48 +52,59 @@ if [[ "${CIME_MODEL}" == "e3sm" ]]
 then
     echo "Setting up E3SM"
 
-    git clone -b ${E3SM_BRANCH:-master} ${GIT_FLAGS} ${E3SM_REPO:-https://github.com/E3SM-Project/E3SM} "${SRC_PATH}/E3SM"
+    [[ ! -e "${SRC_PATH}/E3SM" ]] && git clone -b ${E3SM_BRANCH:-master} ${GIT_FLAGS} ${E3SM_REPO:-https://github.com/E3SM-Project/E3SM} "${SRC_PATH}/E3SM"
 
     pushd "${SRC_PATH}/E3SM"
 
     git config --global --add safe.directory "${PWD}"
 
+    # fix E3SM gitmodules
     fix_gitmodules "${PWD}"
 
     git status
 
+    # checkout submodules
     git submodule update --init "${GIT_SUBMODULE_FLAGS}"
 
+    # fix mct arflags flags
     fix_mct_arflags "${SRC_PATH}/E3SM/externals/mct"
 
     pushd cime
 
+    # fix CIME gitmodules
     fix_gitmodules "${PWD}"
 
+    git config --global --add safe.directory "${PWD}"
     git config --global --add safe.directory "${PWD}/CIME/non_py/cprnc"
 
+    # checkout submodules
     git submodule update --init "${GIT_SUBMODULE_FLAGS}"
 
+    # link v2 config_machines
     ln -sf /root/.cime/config_machines.v2.xml /root/.cime/config_machines.xml
 elif [[ "${CIME_MODEL}" == "cesm" ]]
 then
     echo "Setting up CESM"
 
+    # copy pre cloned repos to new source path
     if [[ "${SRC_PATH}" != "/src/cime" ]]
     then
-        cp -rf /src/ccs_config /src/components /src/libraries /src/share ../
+        cp -rf /src/ccs_config /src/components /src/libraries /src/share "${SRC_PATH}/../"
     fi
 
     git config --global --add safe.directory "${PWD}"
-
     git config --global --add safe.directory "${PWD}/CIME/non_py/cprnc"
 
+    # fix CIME gitmodules
     fix_gitmodules "${PWD}"
 
+    # update CIME submodules
     git submodule update --init "${GIT_SUBMODULE_FLAGS}"
 
+    # fix mct argflags
     fix_mct_arflags /src/libraries/mct
 
+    # link v3 config_machines
     ln -sf /root/.cime/config_machines.v3.xml /root/.cime/config_machines.xml
 fi
 
