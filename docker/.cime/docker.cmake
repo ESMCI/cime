4e2b0d3952c4d3fe3235e67f084849120c7c3386
Jason Boutte <boutte3@llnl.gov>
2025-06-06 12:09:59 -0700

    Updates container

--- docker/.cime/docker.cmake
+++ docker/.cime/docker.cmake
@@ -1,5 +1,21 @@
-string(APPEND CXXFLAGS " -std=c++14")
-string(APPEND CXX_LIBS " -lstdc++")
+
+if (COMP_NAME STREQUAL gptl)
+    string(APPEND CPPDEFS " -DHAVE_NANOTIME _DBIT64 -DHAVE_SLASHPROC -DHAVE_GETTIMEOFDAY")
+endif()
+
+string(APPEND CMAKE_C_FLAGS_RELEASE " -O2 -g")
+string(APPEND CMAKE_Fortran_FLAGS_RELEASE " -O2 -g")
+string(APPEND CMAKE_CXX_FLAGS_RELEASE " -O2 -g")
+
+string(APPEND CMAKE_C_FLAGS_DEBUG " -O0")
+string(APPEND CMAKE_Fortran_FLAGS_DEBUG " -O0")
+string(APPEND CMAKE_CXX_FLAGS_DEBUG " -O0")
+
+# required for grid generation tests that use make
+if (CMAKE_SOURCE_DIR MATCHES "^.*TestGridGeneration.*$")
+    string(APPEND FFLAGS " -I/opt/conda/include")
+    string(APPEND SLIBS " -L/opt/conda/lib -lnetcdf -lnetcdff")
+endif()
 
 # DEBUGGING variables
 # get_cmake_property(_variableNames VARIABLES)
@@ -7,9 +23,3 @@ string(APPEND CXX_LIBS " -lstdc++")
 #     message("${_variableName}=${${_variableName}}")
 # endforeach()
 # message( FATAL_ERROR "EXIT")
-
-# required for grid generation tests that use make
-if (CMAKE_SOURCE_DIR MATCHES "^.*TestGridGeneration.*$")
-string(APPEND FFLAGS " -I/opt/conda/include")
-string(APPEND SLIBS " -L/opt/conda/lib -lnetcdf -lnetcdff")
-endif()
