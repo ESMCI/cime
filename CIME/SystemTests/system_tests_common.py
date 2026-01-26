266badbc24fb4e8ccef96598973a1d783cdbbc3f
James Foucar <jgfouca@sandia.gov>
2026-01-26 14:05:05 -0700

    Add newlines

--- CIME/SystemTests/system_tests_common.py
+++ CIME/SystemTests/system_tests_common.py
@@ -1025,9 +1025,9 @@ for some of your components.
             ts_comments = (
                 os.path.dirname(baseline_name) + ": " + get_ts_synopsis(comments)
             )
-            log_comments = "============ BASELINE COMPARE SYNOPSIS ============="
-            log_comments += ts_comments
-            log_comments += "===================================================="
+            log_comments = "\n\n============ BASELINE COMPARE SYNOPSIS =============\n"
+            log_comments += ts_comments + "\n"
+            log_comments += "====================================================\n"
             append_testlog(log_comments, self._orig_caseroot)
             self._test_status.set_status(BASELINE_PHASE, status, comments=ts_comments)
 
