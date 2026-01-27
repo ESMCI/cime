52c1103bd0e6765d3d951490d0ac2baf7936d28f
Jason Boutte <boutte3@llnl.gov>
2026-01-26 23:00:09 -0800

    Move archive spec and rpointer parsing methods to ArchiveBase

--- CIME/XML/env_archive.py
+++ CIME/XML/env_archive.py
@@ -16,23 +16,5 @@ class EnvArchive(ArchiveBase, EnvBase):
         schema = os.path.join(utils.get_schema_path(), "env_archive.xsd")
         EnvBase.__init__(self, case_root, infile, schema=schema, read_only=read_only)
 
-    def get_archive_specs(self):
-        components_element = self.get_child("components")
-
-        return self.get_children("comp_archive_spec", root=components_element)
-
-    def get_rpointer_nodes(self, root):
-        assert root.name == "comp_archive_spec"
-
-        return self.get_children("rpointer", root=root)
-
-    def get_rpointers(self, root):
-        for node in self.get_rpointer_nodes(root):
-            file = self.get_child("rpointer_file", root=node).text
-
-            content = self.get_child("rpointer_content", root=node).text
-
-            yield file, content
-
     def get_type_info(self, vid):
         return "char"
