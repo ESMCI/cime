3bb231c0747cbd6b2bfad64783003029349d0fbe
Jason Boutte <boutte3@llnl.gov>
2026-02-10 11:22:58 -0800

    Cleans up entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
-export USER=root
-export LOGNAME=root
-export USER_ID=${USER_ID:-1000}
-export GROUP_ID=${GROUP_ID:-1000}
+# Set up basic user, logname, and default group/user IDs
+export USER=`id -nu`
+export LOGNAME="${USER}"
+export USER_ID="${USER_ID:-1000}"
+export GROUP_ID="${GROUP_ID:-1000}"
 
+# Set static home path where .cime exists and container entrypoint options
+CIME_HOME="/home/cime"
 SKIP_ENTRYPOINT="${SKIP_ENTRYPOINT:-false}"
+SRC_PATH="${SRC_PATH:-${PWD}}"
 
+
+# Determine HOME directory: use provided HOME in CI, /root if root, default otherwise
+if [[ "${CI:-false}" == "true" ]]; then
+    HOME="${HOME}"
+elif [[ "${USER_ID}" == "0" ]]; then
+    HOME="/root"
+else
+    # Not populated in $HOME until gosu switches user
+    HOME="/home/cime"
+fi
+
+
+# Fix AR variable in MCT-related Makefiles
 function fix_mct_makefiles() {
     fix_arflags "${1}/mct/Makefile"
     fix_arflags "${1}/mpeu/Makefile"
     fix_arflags "${1}/mpi-serial/Makefile"
 }
 
+
+# Add ARFLAGS to AR in a Makefile if .bak does not exist
 function fix_arflags() {
     if [[ ! -e "${1}.bak" ]]; then
         echo "Fixing AR variable in ${1}"
@@ -21,8 +40,15 @@ function fix_arflags() {
     fi
 }
 
+
+# Build the cprnc tool from CIME sources
 function build_cprnc() {
-    cprnc_dir="${SRC_PATH:-`pwd`}/CIME/non_py/cprnc"
+    cprnc_dir="${PWD}/CIME/non_py/cprnc"
+
+    if [[ ! -e "${cprnc_dir}" ]]; then
+        echo "CPRNC path does not exist. Change to CIME's root directory."
+        exit 0
+    fi
 
     pushd `mktemp -d`
 
@@ -30,48 +56,53 @@ function build_cprnc() {
 
     make
 
-    cp cprnc /home/cime/tools/cprnc
+    # Needs to be copied into the machines configured tool path
+    cp cprnc "${CIME_HOME}/tools/cprnc"
 
     popd
 }
 
-function download_input_data() {
-    mkdir -p /home/cime/inputdata/cpl/gridmaps/oQU240 \
-        /home/cime/inputdata/share/domains \
-        /home/cime/timings \
-        /home/cime/cases \
-        /home/cime/archive \
-        /home/cime/baselines \
-        /home/cime/tools
 
-    wget -O /home/cime/inputdata/cpl/gridmaps/oQU240/map_oQU240_to_ne4np4_aave.160614.nc \
+# Download input data needed for model setup
+function download_input_data() {
+    mkdir -p "${CIME_HOME}/inputdata/cpl/gridmaps/oQU240" \
+        "${CIME_HOME}/inputdata/share/domains" \
+        "${CIME_HOME}/timings" \
+        "${CIME_HOME}/cases" \
+        "${CIME_HOME}/archive" \
+        "${CIME_HOME}/baselines" \
+        "${CIME_HOME}/tools"
+
+    wget -O "${CIME_HOME}/inputdata/cpl/gridmaps/oQU240/map_oQU240_to_ne4np4_aave.160614.nc" \
         https://portal.nersc.gov/project/e3sm/inputdata/cpl/gridmaps/oQU240/map_oQU240_to_ne4np4_aave.160614.nc
 
-    wget -O /home/cime/inputdata/share/domains/domain.ocn.ne4np4_oQU240.160614.nc \
+    wget -O "${CIME_HOME}/inputdata/share/domains/domain.ocn.ne4np4_oQU240.160614.nc" \
         https://portal.nersc.gov/project/e3sm/inputdata/share/domains/domain.ocn.ne4np4_oQU240.160614.nc
 
-    wget -O /home/cime/inputdata/share/domains/domain.lnd.ne4np4_oQU240.160614.nc \
+    wget -O "${CIME_HOME}/inputdata/share/domains/domain.lnd.ne4np4_oQU240.160614.nc" \
         https://portal.nersc.gov/project/e3sm/inputdata/share/domains/domain.lnd.ne4np4_oQU240.160614.nc
 }
 
+
+# Link correct config_machines file based on CIME_MODEL, also set ESMFMKFILE for cesm
 function link_config_machines() {
     if [[ "${CIME_MODEL}" == "e3sm" ]]; then
-        ln -sf ${1}/.cime/config_machines.v2.xml ${1}/.cime/config_machines.xml
+        ln -sf "${CIME_HOME}/.cime/config_machines.v2.xml" "${HOME}/.cime/config_machines.xml"
     elif [[ "${CIME_MODEL}" == "cesm" ]]; then
         export ESMFMKFILE=/opt/conda/envs/cesm/lib/esmf.mk
 
-        ln -sf ${1}/.cime/config_machines.v3.xml ${1}/.cime/config_machines.xml
+        ln -sf "${CIME_HOME}/.cime/config_machines.v3.xml" "${HOME}/.cime/config_machines.xml"
     fi
 }
 
-user_home=/home/cime
 
-if [[ "${USER_ID}" == "0" ]]; then
-    user_home=/root
-
-    cp -rf /home/cime/.cime /root/
+# If root or in CI, move .cime config to the appropriate home directory
+if [[ "${USER_ID}" == "0" ]] || [[ "${CI:-false}" == "true" ]]; then
+    cp -rf "${CIME_HOME}/.cime" "${HOME}"
 fi
 
+
+# Write minimal .bashrc to activate correct conda environment and ensure system perl is preferred
 {
     echo "source /opt/conda/etc/profile.d/conda.sh"
     echo "conda activate base"
@@ -80,35 +111,31 @@ fi
     echo "elif [[ ${CIME_MODEL} == 'cesm' ]]; then"
     echo "  conda activate cesm"
     echo "fi"
-    # need this to perfer host perl over conda
+    # Clobber PATH so system perl is used instead of conda perl
     echo "export PATH=/usr/bin:\$PATH"
-} > ${user_home}/.bashrc
+} > "${HOME}/.bashrc"
 
-link_config_machines ${user_home}
 
-if [[ -e "${PWD}/.git" ]]; then
+link_config_machines
+
+# Allow git to operate in any directory, for container/dev scenarios
+if [[ -e "${SRC_PATH}/.git" ]]; then
     git config --global --add safe.directory "*"
-elif [[ -n "${SRC_PATH}" ]]; then
-    pushd "${SRC_PATH}"
-    git config --global --add safe.directory "*"
-    popd
 fi
 
+
+# If not skipping entrypoint, set up user/group IDs and exec given command.
 if [[ "${SKIP_ENTRYPOINT}" == "false" ]]; then
     if [[ "${USER_ID}" == "0" ]]; then
         exec "${@}"
     else
+        # Modify user/group, useful for local in-container development
         groupmod -g "${GROUP_ID}" -n cime ubuntu
-        usermod -d /home/cime -u "${USER_ID}" -g "${GROUP_ID}" -l cime ubuntu
+        usermod -d "${HOME}" -u "${USER_ID}" -g "${GROUP_ID}" -l cime ubuntu
 
-        chown -R cime:cime /home/cime
-        chmod -R 775 /home/cime
-
-        if [[ -n "${SRC_PATH}" ]] && [[ -e "${SRC_PATH}" ]]; then
-            chown -R cime:cime "${SRC_PATH}"
-            chmod -R 775 "${SRC_PATH}"
-        fi
+        chown -R cime:cime "${HOME}"
 
         gosu "${USER_ID}" "${@}"
     fi
 fi
+
