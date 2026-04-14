c19d517cb6f881d96d0cd272cbfa1d0b03105610
Michael Levy <mike.levy.work@gmail.com>
2026-04-14 08:30:09 -0600

    Add a couple short comments to new test module

--- CIME/tests/test_simple_compare.py
+++ CIME/tests/test_simple_compare.py
@@ -7,11 +7,15 @@ class TestSimpleCompare(unittest.TestCase):
     def test_normalize_string_values(self):
         test = "test.grid.compset.mach_compiler"
         testid = "19991231_235959_abcdef"
+
+        # Test cases where _normalize_string_value() will normalize test
         for action in ["G", "C", "GC"]:
             assert (
                 _normalize_string_value(f"{test}.{action}.{testid}", test)
                 == f"{test}.ACTION.TESTID"
             )
+
+        # Test cases where _normalize_string_value() will not change test
         for action in ["GG", "CC", "CG"]:
             assert (
                 _normalize_string_value(f"{test}.{action}.{testid}", test)
