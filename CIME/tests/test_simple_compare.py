3700d301aa262da5a8063ac3ab9994e4f81bcac4
Michael Levy <mike.levy.work@gmail.com>
2026-04-14 08:26:40 -0600

    Updates from black via pre-commit

--- CIME/tests/test_simple_compare.py
+++ CIME/tests/test_simple_compare.py
@@ -7,5 +7,8 @@ class TestSimpleCompare(unittest.TestCase):
     def test_normalize_string_values(self):
         test = "test.grid.compset.mach_compiler"
         testid = "19991231_235959_abcdef"
-        for action in ['G', 'C', 'GC']:
-            assert(_normalize_string_value(f"{test}.{action}.{testid}", test) == f"{test}.ACTION.TESTID")
+        for action in ["G", "C", "GC"]:
+            assert (
+                _normalize_string_value(f"{test}.{action}.{testid}", test)
+                == f"{test}.ACTION.TESTID"
+            )
